688647377a0c721622c6c483583986d0cb19b2a8
Rachana Prakash <rachana.prakash91@gmail.com>
2023-06-23 22:57:57 +0530

    Installing Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div> App </div>;
+  return <div></div>;
 }
 
 export default App;
