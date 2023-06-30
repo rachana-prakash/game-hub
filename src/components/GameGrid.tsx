01cff9d4a406fe30e18a0c545c4d71dac97d9682
Rachana Prakash <rachana.prakash91@gmail.com>
2023-06-30 20:09:37 +0530

    Display Genre List and refactor data hooks

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,24 +5,24 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={4}
         padding={10}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer>
+            <GameCardContainer key={skeleton}>
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((gameItem) => (
-          <GameCardContainer>
+        {data.map((gameItem) => (
+          <GameCardContainer key={gameItem.id}>
             <GameCard key={gameItem.id} game={gameItem} />
           </GameCardContainer>
         ))}
