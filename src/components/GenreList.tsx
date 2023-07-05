3de5718631d6693be5167bfca2dd777b4d9ba99c
Rachana Prakash <rachana.prakash91@gmail.com>
2023-07-05 19:24:09 +0530

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -23,6 +24,9 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   } else {
     return (
       <>
+        <Heading fontSize="2xl" marginBottom={3}>
+          Genres
+        </Heading>
         <List>
           {data.map((genre) => (
             <ListItem key={genre.id} paddingY="5px">
@@ -30,9 +34,12 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
                 <Image
                   boxSize="32px"
                   borderRadius={8}
+                  objectFit="cover"
                   src={getCroppedImageUrl(genre.image_background)}
                 />
                 <Button
+                  whiteSpace="normal"
+                  textAlign="left"
                   fontWeight={
                     genre.id === selectedGenre?.id ? "bold" : "normal"
                   }
